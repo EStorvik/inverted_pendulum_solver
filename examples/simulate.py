1a04f353f490b80b3654cc23f9b9d5f1a0e3d133
EStorvik <erlend.storvik@hvl.no>
2024-04-08 12:24:00 +0200

    Updated mistakes and tested

--- examples/simulate.py
+++ examples/simulate.py
@@ -3,7 +3,9 @@ import inverted_pendulum as ip
 import control as ct
 
 r = 1
-L = 1
+m_theta = 1
+m_alpha = 0.5
+L = 2
 g = 9.81
 
 theta_0 = 0
@@ -13,8 +15,8 @@ alpha_0 = 0
 x0 = np.array([theta_0, theta_dot_0, alpha_0, alpha_dot_0])
 
 # Control using the python control library
-A = np.array([[0,1,0,0],[0,0,3*g/L,0],[0,0,0,1],[0,0,6*g/L,0]])
-B = np.array([[0],[2/r**2],[0],[3/(L*r)]])
+A = np.array([[0,1,0,0],[0,0,m_alpha*g/(4*r*(m_alpha/4+m_theta/3)),0],[0,0,0,1],[0,0,3*g*(m_alpha+m_theta/3)/(2*L*(m_alpha/4+m_theta/3)),0]])
+B = np.array([[0],[1/(r**2*(m_alpha/4+m_theta/3))],[0],[3/(2*L*r*(m_alpha/4+m_theta/3))]])
 Q = np.eye(4)
 R = np.eye(1)
 
@@ -24,7 +26,7 @@ control = ip.BalanceControl(x0, A, B, Q, R)
 tau = control.swing_up
 
 def rhs(x):
-    return ip.rhs(x, tau, r, L, g)
+    return ip.rhs(x, tau, r, L, m_theta, m_alpha, g)
 
 h = 0.01
 x, t = ip.rk4(rhs, 0, x0, 20, h)
